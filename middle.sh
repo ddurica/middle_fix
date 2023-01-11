ab92d23b5d8408deabf173d8267c093b62f084fc
David Durica <ddurica@ou.edu>
2023-01-11 16:56:14 +0000

    added variables for number of lines

--- middle.sh
+++ middle.sh
@@ -2,5 +2,7 @@
 #script to list the middle of a file
 #$1 represents the filename
 # usage:
-#bash middle.sh [filename] 
+#bash middle.sh [filename] [head number of lines][tail number of lines]
+# example
+#bash middle.sh unicorn 50 7 
 head -n 75  $1 | tail -n 5
